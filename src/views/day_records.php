c0f1da11975e559c46464fbb23c014371455718e
EddieSCJ <EddieStudyIfs@gmail.com>
2020-02-07 14:02:58 -0300

    LUPE - Fazer tela de bater ponto

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,37 +1,30 @@
 <main class="content">
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
-    <h1>ok</h1>
+    <div class="content-title mb-4">
+        <i class="icon icofont-check-alt mr-2"></i>
+        <div>
+            <h1>Registrar ponto</h1>
+            <h2>Mantenha seu ponto consistente</h2>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h3>10 de Janeiro de 2025</h3>
+            <p>Os batientos efetutuados hoje</p>
+        </div>
+        <div class="card-body ">
+            <div class="d-flex m-5 justify-content-around">
+                <span>Entrada 1: ----</span>
+                <span>Saída 1: ----</span>
+            </div>
+            <div class="d-flex m-5 justify-content-around">
+                <span>Entrada 2: ----</span>
+                <span>Saída 2: ----</span>
+            </div>
+        </div>
+        <div class="card-footer d-flex justify-content-center align-items-center">
+            <a href="???" class="btn btn-success btn-md">
+                <i class="icofont-check mr-1">Bater o ponto</i>
+            </a>
+        </div>
+    </div>
 </main>
